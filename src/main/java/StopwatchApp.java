d23fda3522ea481ec23a51f4525a4195bfe697fa
abrarafnansajib3 <abrarafnansajib3@gmail.com>
2024-01-21 01:10:17 +0600

    project uploaded

---
+++ src/main/java/StopwatchApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Afnan60
+ */
+
+public class StopwatchApp {
+    // Other code...
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            NewJFrame frame = new NewJFrame();
+            frame.setVisible(true);
+        });
+    }
+}
